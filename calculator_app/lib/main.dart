27d1c7d166407a3b79b05f30f9df29ff16d41fcf
interrrpol <105016665+interrrpol@users.noreply.github.com>
2022-05-29 16:16:01 +0300

    calculator_start

---
+++ calculator_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SimpleCalculator(),
+    );
+  }
+}
