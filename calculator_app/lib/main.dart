941f4038b3fd8075e3cca8e7eacd6229401f6a54
Evelyne03 <evelyne.tudureanu03@e-uvt.ro>
2022-05-29 16:35:42 +0300

    button1

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -15,3 +15,25 @@ class Calculator extends StatelessWidget {
     );
   }
 }
+
+class SimpleCalculator extends StatefulWidget {
+  @override
+  _SimpleCalculatorState createState() => _SimpleCalculatorState();
+}
+
+class _SimpleCalculatorState extends State<SimpleCalculator> {
+  String equation = "0";
+  String result = "0";
+  String expression = "";
+  double equationFontSize = 38.0;
+  double resultFontSize = 48.0;
+
+  buttonPressed(String buttonText){
+    setState(() {
+      if(buttonText == "C"){
+        equation = "0";
+        result = "0";
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
+      }
+}
