547a2396c4ebfb8b4b613164283f6fce3941048c
interrrpol <105016665+interrrpol@users.noreply.github.com>
2022-05-29 16:39:09 +0300

    button2

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -36,4 +36,33 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
         equationFontSize = 38.0;
         resultFontSize = 48.0;
       }
-}
+    }
+
+      else if(buttonText == "⌫"){
+        equationFontSize = 48.0;
+        resultFontSize = 38.0;
+        equation = equation.substring(0, equation.length - 1);
+        if(equation == ""){
+          equation = "0";
+        }
+      }
+
+      else if(buttonText == "="){
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
+
+        expression = equation;
+        expression = expression.replaceAll('×', '*');
+        expression = expression.replaceAll('÷', '/');
+
+        try{
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+
+          ContextModel cm = ContextModel();
+          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+        }catch(e){
+          result = "Error";
+        }
+
+      }
